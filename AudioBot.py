89a3d557099850de8ef504a1c96b06a703aa0976
Aynakeya <aynakeya@aynakeya.com>
2021-03-13 05:15:19 +0800

    demo 0.7.0 web

--- AudioBot.py
+++ AudioBot.py
@@ -1,9 +1,12 @@
 import os
 from threading import Thread
 
+from aiohttp import web
+
 os.environ["PATH"] = os.getcwd() + os.pathsep + os.environ["PATH"]
 
 from gui import MainWindow
+from backend import aioserver
 import asyncio
 import nest_asyncio
 nest_asyncio.apply()
@@ -11,7 +14,14 @@ nest_asyncio.apply()
 
 async def mainloop(loop):
     a = MainWindow()
-    await asyncio.create_task(a.start())
+
+    await asyncio.create_task(a.start(),
+                              run_backend())
+
+def run_backend():
+    backend = aioserver.app
+    web.run_app(backend, host='127.0.0.1', port=5000)
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
---
+++ backend/aioserver.py
@@ -0,0 +1,27 @@
+import aiohttp
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+ENV = "development"
+
+app = web.Application()
+routes = web.RouteTableDef()
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader('./frontend_dist'))
+
+@routes.get("/")
+@routes.get("/{path:.*}")
+async def vue_redirecting(request:web.Request):
+    try:
+        path = request.match_info["path"]
+    except:
+        path = ""
+    if ENV == "development":
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://localhost:8080/{}'.format(path)) as resp:
+                return web.Response(text=await resp.text(),
+                                    content_type= resp.content_type)
+    return aiohttp_jinja2.render_template('index.html',
+                                              request)
+
+app.router.add_routes(routes)
