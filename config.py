abc0182fc8a0482df2f4aa530dafd3c3820be305
Aynakeya <aynakeya@aynakeya.com>
2022-01-10 12:32:59 -0800

    fix netease api error

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class ConfigFile:
 
     environment = "production"
     # environment = "development"
-    version = "Demo0.8.8"
+    version = "Demo0.8.91"
 
     commonHeaders = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0"
